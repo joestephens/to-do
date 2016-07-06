311106caeffade997e036ba07d511889d5d3c8aa
joestephens <joe.stephens@live.co.uk>
2016-07-06 10:20:45 +0100

    Zombie setup

--- test/featureTest.js
+++ test/featureTest.js
@@ -1,14 +1,16 @@
+var app = require('http-server').createServer();
 var Browser = require('zombie');
-var createServer = require('http-server').createServer;
-var path = require('path');
-var server = createServer({root: path.join(__dirname, "../")});
-
-server.listen(3000);
-Browser.localhost('localhost
-', 3000);
+var assert = require('chai').assert;
 
 describe('user visits page', function() {
-  var browser = new Browser();
+  before(function() {
+    server = app.listen(8080);
+    browser = new Browser({ site: 'http://localhost:8080' })
+  });
+
+  beforeEach(function(done) {
+    browser.visit('/', done);
+  });
 
   it('should have a title', function() {
     browser.assert.text('title', 'To Do List');
