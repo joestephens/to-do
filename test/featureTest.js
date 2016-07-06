966ac3918b1c39198ad279aed3c21df7091f6737
joestephens <joe.stephens@live.co.uk>
2016-07-06 10:30:43 +0100

    Fixed zombie

--- test/featureTest.js
+++ test/featureTest.js
@@ -1,11 +1,11 @@
 var app = require('http-server').createServer();
-var Browser = require('zombie');
 var assert = require('chai').assert;
+var Browser = require('zombie');
 
 describe('user visits page', function() {
   before(function() {
-    server = app.listen(8080);
-    browser = new Browser({ site: 'http://localhost:8080' })
+    server = app.listen(3000);
+    browser = new Browser({ site: 'http://localhost:3000' })
   });
 
   beforeEach(function(done) {
